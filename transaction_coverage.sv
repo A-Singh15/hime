b6d178b5e6aa4b501f5d1085997642724b331207
Aaron <70713574+A-Singh15@users.noreply.github.com>
2024-05-24 14:01:27 -0700

    Update transaction_coverage.sv

--- transaction_coverage.sv
+++ transaction_coverage.sv
@@ -1,7 +1,10 @@
 `timescale 1ns/1ps
 
-`define RMEM_MAX 256
 `define SMEM_MAX 1024
+`define RMEM_MAX 256
+`define TRANSACTION_COUNT 500
+`define DRIV_IF memoryInterface.DriverInterface.Driver_cb
+`define MON_IF memoryInterface.MonitorInterface.Monitor_cb
 
 class Transaction;
 
